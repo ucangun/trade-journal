6d625328f2a48230277d6a904a63571595ca536e
ucangun <149247682+ucangun@users.noreply.github.com>
2025-04-16 11:31:32 +0200

    feat: implement routing with Home and PrivateRouter components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1 className="">Welcome Trade Journal</h1>;
+};
+
+export default Home;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrivateRouter from "./PrivateRouter";
+import Home from "../pages/Home";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* Public Area */}
+        <Route path="/" element={<Home />} />
 
         {/* Private Area */}
         <Route element={<PrivateRouter />}></Route>
