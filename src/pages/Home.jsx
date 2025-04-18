91adffddbc571cfe2253f622d4e9f68f0552fc73
ucangun <149247682+ucangun@users.noreply.github.com>
2025-04-18 18:24:46 +0200

    feat: update index.html title, add Navbar to Home page, and implement Login and Register pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,18 @@ import CallToAction from "../components/Home/CallToAction";
 import Features from "../components/Home/Features";
 import Hero from "../components/Home/Hero";
 import Testimonials from "../components/Home/Testimonials";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <>
+    <div className="min-h-screen bg-white">
+      <Navbar />
       <Hero />
       <Features />
       <Testimonials />
       <CallToAction />
       <Footer />
-    </>
+    </div>
   );
 };
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrivateRouter from "./PrivateRouter";
 import Home from "../pages/Home";
-import Navbar from "../components/Navbar";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         {/* Public Area */}
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/signin" element={<Login />} />
 
         {/* Private Area */}
         <Route element={<PrivateRouter />}></Route>
