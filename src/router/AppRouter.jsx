13e747de90d6ddc3abaf803a6c43d14e2264cc1a
ucangun <149247682+ucangun@users.noreply.github.com>
2025-04-18 22:13:43 +0200

    feat: add Profile page and implement PrivateRouter for authentication checks

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import PrivateRouter from "./PrivateRouter";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import Profile from "../pages/Profile";
 
 const AppRouter = () => {
   return (
@@ -14,7 +15,9 @@ const AppRouter = () => {
         <Route path="/signin" element={<Login />} />
 
         {/* Private Area */}
-        <Route element={<PrivateRouter />}></Route>
+        <Route element={<PrivateRouter />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,5 +1,20 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { toastErrorNotify } from "../helpers/toastNotify";
+import { useEffect } from "react";
+
 const PrivateRouter = () => {
-  return <div>PrivateRouter</div>;
+  const navigate = useNavigate();
+  const { currentUser } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!currentUser) {
+      toastErrorNotify("You must be logged in to access this page.");
+      navigate("/login");
+    }
+  }, [currentUser, navigate]);
+
+  return currentUser ? <Outlet /> : null;
 };
 
 export default PrivateRouter;
