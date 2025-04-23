5f662331c0999c1a17bf904578000a4f9dbe61e3
ucangun <149247682+ucangun@users.noreply.github.com>
2025-04-23 21:30:07 +0200

    feat: add redux-persist for state persistence in auth reducer

--- src/app/store.js
+++ src/app/store.js
@@ -5,14 +5,42 @@ import capitalDepositReducer from "../features/capitalDepositSlice";
 import transactionReducer from "../features/transactionSlice";
 import userReducer from "../features/userSlice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     stock: stockReducer,
     capitalDeposit: capitalDepositReducer,
     transaction: transactionReducer,
     user: userReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export default store;
